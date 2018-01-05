1fa30e6ec48d7c671465e8655eb639bda2e2d6ec
Brian Chirls <brian@chirls.com>
2018-01-05 16:08:25 -0500

    More compact syntax ...matches eslint documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,64 +19,29 @@ module.exports = {
 		"no-extend-native": 2,
 		"block-scoped-var": 2,
 		"strict": 1,
-		"wrap-iife": [
-			2,
-			"any"
-		],
+		"wrap-iife": [ 2, "any" ],
 		"no-iterator": 2,
-		"no-use-before-define": [
-			2,
-			{
-				"functions": false
-			}
-		],
-		"max-params": [
-			2,
-			5
-		],
+		"no-use-before-define": [ 2, { "functions": false } ],
+		"max-params": [ 2, 5 ],
 		"new-cap": 2,
 		"no-caller": 2,
 		"no-irregular-whitespace": 2,
 		"no-new": 2,
 		"valid-typeof": 2,
-		"quotes": [
-			2,
-			"single"
-		],
+		"quotes": [ 2, "single" ],
 		"no-shadow": 0,
 		"no-extra-parens": 0,
 		"no-undef": 2,
 		"no-unused-vars": 2,
-		"indent": [
-			"error",
-			"tab",
-			{
-				"SwitchCase": 1
-			}
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"eol-last": [
-			"off"
-		],
+		"indent": [ "error", "tab", { "SwitchCase": 1 } ],
+		"linebreak-style": [ "error", "unix" ],
+		"eol-last": [ "off" ],
 		"no-implicit-globals": 2,
 		"prefer-const": 1,
-		"one-var": [
-			1,
-			"never"
-		],
+		"one-var": [ 1, "never" ],
 		"one-var-declaration-per-line": 1,
 		"no-loop-func": 2,
-		"space-before-function-paren": [
-			"error",
-			{
-				"anonymous": "always",
-				"named": "never",
-				"asyncArrow": "always"
-			}
-		],
+		"space-before-function-paren": [ "error", { "anonymous": "always", "named": "never", "asyncArrow": "always" } ],
 		"no-plusplus": 0,
 		"require-await": 2
 	}
