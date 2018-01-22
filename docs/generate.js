800109ced4776ad912a63d8fbede0f2052323592
Brian Chirls <brian@chirls.com>
2018-01-22 12:27:33 -0500

    Build documentation of code style

---
+++ docs/generate.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const Linter = require('eslint').Linter;
+const linter = new Linter();
+
+const categories = new Map();
+
+let header = '';
+let rest = '';
+function finish() {
+	if (header && rest) {
+		fs.writeFile(__dirname + '/../readme.md', header + rest, err => {
+			if (err) {
+				throw err;
+			}
+			console.log('Code style documentation generated');
+		});
+	}
+}
+
+fs.readFile(__dirname + '/header.md', (err, data) => {
+	if (err) {
+		throw err;
+	}
+	header = data;
+	finish();
+});
+
+const rules = linter.getRules();
+rules.forEach((rule, key) => {
+	const { docs } = rule.meta;
+	const { category } = docs;
+	if (!categories.has(category)) {
+		categories.set(category, []);
+	}
+	categories.get(category).push({ key, docs });
+});
+
+categories.forEach((rules, name) => {
+	rest += `\n### ${name}\n`;
+	rest += rules.map(rule => `[${rule.key}](https://eslint.org/docs/rules/${rule.key}) | ${rule.docs.description}`).join('\n');
+});
+finish();
---
+++ docs/header.md
@@ -0,0 +1,11 @@
+# Datavized Code Style
+
+Our code style is based on Crockfor's, with a few modifications. We use 
+
+## The Basics
+- Indent with tabs, not spaces
+- Always end statements with semicolons
+- Conditionals and loops ("if", "while", "for") always use curly braces
+- Never use `var`. Use `const` wherever possible and `let` only if necessary.
+
+## The Rules
